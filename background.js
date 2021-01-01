a586412b6bce73ac33576e31e2a308e541ec880c
Tomohiro Imaizumi <bonriki.life@gmail.com>
2021-01-02 02:14:14 +0900

    fix

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({width: 1080}, function() {
+    console.log('Set width to 1080px.');
+  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
