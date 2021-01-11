9eca592e445e7d586e1ec37e32f8b7b5e7d5a766
Tomohiro Imaizumi <bonriki.life@gmail.com>
2021-01-12 00:01:36 +0900

    Set default width for enabled to 1200px

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.browserAction.setIcon({ path:  "images/inactive.png" });
+  chrome.storage.sync.set({ 'width': 1200 });
+});
 
-chrome.browserAction.onClicked.addListener(
-  function(tab){
-    chrome.storage.sync.get('isEnabled', function(data) {
-      let newState = !data.isEnabled
-      chrome.storage.sync.set(
-        { isEnabled: newState },
-        function() {
-          let iconPath = newState ? "images/icon128.png" : "images/inactive.png";
-          chrome.browserAction.setIcon({ path: iconPath });
-          chrome.tabs.sendMessage(tab.id, {});
-        }
-      );
-    });
-  }
-);
+chrome.browserAction.onClicked.addListener(function(tab){
+  chrome.storage.sync.get('isEnabled', function(data) {
+    let newState = !data.isEnabled
+    chrome.storage.sync.set(
+      { isEnabled: newState },
+      function() {
+        let iconPath = newState ? "images/icon128.png" : "images/inactive.png";
+        chrome.browserAction.setIcon({ path: iconPath });
+        chrome.tabs.sendMessage(tab.id, {});
+      }
+    );
+  });
+});
 
 chrome.tabs.onUpdated.addListener(
   function(tabId, changeInfo, tab) {
