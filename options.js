f623455fad8931312efc66ede1daf302e095e69a
Tomohiro Imaizumi <bonriki.life@gmail.com>
2021-01-11 15:35:29 +0900

    Refactor building alert

--- options.js
+++ options.js
@@ -2,32 +2,47 @@
 
 let page = document.getElementById('form');
 
-function createAlert(isSuccess) {
-  let cls = isSuccess ? 'uk-alert-success' : 'uk-alert-danger';
+function createAlert(option) {
   let parent = document.createElement('div');
-  parent.classList.add(cls);
+  parent.classList.add(option.class);
   parent.setAttribute('uk-alert', '');
   let closeBtn = document.createElement('a');
   closeBtn.classList.add('uk-alert-close');
   closeBtn.setAttribute('uk-close', '');
   parent.appendChild(closeBtn);
   let p = document.createElement('p');
-  let text = isSuccess
-    ? "Value is successfully updated!"
-    : "Value is non-numeric or invalid.";
-  let textNode = document.createTextNode(text);
+  let textNode = document.createTextNode(option.text);
   p.appendChild(textNode);
   parent.appendChild(p);
-  return parent
+  return parent;
+}
+
+function isValidWidth(width) {
+  console.info(width);
+  return Number.isInteger(width);
+}
+
+function buildAlertOption(newWidth) {
+  let isSuccess = isValidWidth(newWidth);
+  return {
+    'class': isSuccess ? 'uk-alert-success' : 'uk-alert-danger',
+    'text': isSuccess ? `Value is successfully updated to ${newWidth}!` : "Value is non-numeric or invalid.",
+  };
 }
 
 let button = document.getElementById('button');
-let alrt = document.getElementById('alert');
+let alertArea = document.getElementById('alert');
 button.addEventListener('click', function() {
-  var w = parseInt(document.getElementById('width').value);
-  chrome.storage.sync.set({width: w}, function() {
-    alrt.innerHTML = '';
-    alrt.appendChild(createAlert(Number.isInteger(w)));
-  })
+  let newWidth = parseInt(document.getElementById('width').value);
+  let option = buildAlertOption(newWidth);
+  alertArea.innerHTML = '';
+  if (isValidWidth(newWidth)) {
+    chrome.storage.sync.set({ width: newWidth }, function() {
+      alertArea.appendChild(createAlert(option));
+    })
+    console.info();
+  } else {
+    alertArea.appendChild(createAlert(option));
+  };
 });
 
